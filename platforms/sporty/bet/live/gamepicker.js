fadc9a59789740ceaec76d2cc94b0c8d3a4698e0
kweku kankam <kweku360@gmail.com>
2021-04-07 19:54:24 +0000

    added a  market picker for 1x2

--- platforms/sporty/bet/live/gamepicker.js
+++ platforms/sporty/bet/live/gamepicker.js
@@ -30,52 +30,23 @@ GamePicker.pickGame = async (page) => {
             return wrapper.length;
         }, i);
 
-        for (let j = 0; j < gameCount; j++) {
+        for (let j = 1; j <= gameCount; j++) {
             const currentTime = await timerCheck(i, page);
-
+            await pickMarket(i,j,page);
             //we check if current time is greater than a time value
             if (currentTime > 60) {
-                console.log(`Game time reached  - ${currentTime} minutes`)
+               // console.log(`Game time reached  - ${currentTime} minutes`)
                 // continue
                 // await page.waitForSelector(`.m-overview > .match > .m-table:nth-child(${currentLeaguePick}) > .m-table-row:nth-child(${leagueGamePick}) > .market-size`)
                 // await page.click(`.m-overview > .match > .m-table:nth-child(${currentLeaguePick}) > .m-table-row:nth-child(${leagueGamePick}) > .market-size`)
 
             } else {
-                console.log(`Game time not reached  - ${currentTime} minutes`)
+              //  console.log(`Game time not reached  - ${currentTime} minutes`)
             }
         }
 
 
     }
-
-
-    // //get random number from range of live leagues
-    // const currentLeaguePick = getRandomNum(data - 1, 2);
-
-    // //get count of games available for a picked league
-    // // const gameCount = await page.evaluate((val) => {
-    // //     const wrapper = Array.from(document.querySelectorAll(`.m-overview > .match > .m-table:nth-child(${val}) > .m-table-row`))
-    // //     return wrapper.length;
-    // // }, currentLeaguePick);
-
-    // //get random number from range of games available
-    // const leagueGamePick = getRandomNum(gameCount - 1, 2);
-
-    // //we do some logging
-    // console.log(leagueGamePick)
-    // console.log(currentLeaguePick)
-
-    // const currentTime = await timerCheck(currentLeaguePick, page);
-
-    // //we check if current time is greater than a time value
-    // if (currentTime > 60) {
-    //     // continue
-    //     await page.waitForSelector(`.m-overview > .match > .m-table:nth-child(${currentLeaguePick}) > .m-table-row:nth-child(${leagueGamePick}) > .market-size`)
-    //     await page.click(`.m-overview > .match > .m-table:nth-child(${currentLeaguePick}) > .m-table-row:nth-child(${leagueGamePick}) > .market-size`)
-
-    // } else {
-    //     console.log(`Game time not reached  - ${currentTime} minutes`)
-    // }
 
 }
 
@@ -129,7 +100,60 @@ async function timerCheck(currentLeaguePick, page) {
     return parseInt(tVal[0].trim());
 }
 
+async function pickMarket(currentleague,currentgame, page) {
+    let oddArr = [];
+    const homeOdd  =  await singleOdd(page,currentleague,currentgame,1)
+    const drawOdd  =  await singleOdd(page,currentleague,currentgame,2)
+    const awayOdd  =  await singleOdd(page,currentleague,currentgame,3)
+    oddArr = [homeOdd,drawOdd,awayOdd]
+    console.log(oddArr);
+}
 
+async function singleOdd(page,currentleague,currentgame,outcomechild){
+    // console.log(`${currentleague} ${currentgame} ${outcomechild}`);
+    return await page.evaluate((j, k,l) => {
+        const wrapper = document.querySelector(`.m-table:nth-child(${j}) > .m-table-row:nth-child(${k})> .m-table-cell > .m-market:nth-child(1) > .m-outcome:nth-child(${l}) > .m-outcome-odds`)
+        if (wrapper != null) {
+            let v = parseFloat(wrapper.innerHTML);
+            if (isNaN(v)) {
+                return 0
+            } else {  
 
+                return wrapper.innerHTML;
+            } 
+        } else {
+            return 0;
+        }
+    }, currentleague, currentgame, outcomechild); 
+}
 
 module.exports = GamePicker
+
+
+    // //get random number from range of live leagues
+    // const currentLeaguePick = getRandomNum(data - 1, 2);
+
+    // //get count of games available for a picked league
+    // // const gameCount = await page.evaluate((val) => {
+    // //     const wrapper = Array.from(document.querySelectorAll(`.m-overview > .match > .m-table:nth-child(${val}) > .m-table-row`))
+    // //     return wrapper.length;
+    // // }, currentLeaguePick);
+
+    // //get random number from range of games available
+    // const leagueGamePick = getRandomNum(gameCount - 1, 2);
+
+    // //we do some logging
+    // console.log(leagueGamePick)
+    // console.log(currentLeaguePick)
+
+    // const currentTime = await timerCheck(currentLeaguePick, page);
+
+    // //we check if current time is greater than a time value
+    // if (currentTime > 60) {
+    //     // continue
+    //     await page.waitForSelector(`.m-overview > .match > .m-table:nth-child(${currentLeaguePick}) > .m-table-row:nth-child(${leagueGamePick}) > .market-size`)
+    //     await page.click(`.m-overview > .match > .m-table:nth-child(${currentLeaguePick}) > .m-table-row:nth-child(${leagueGamePick}) > .market-size`)
+
+    // } else {
+    //     console.log(`Game time not reached  - ${currentTime} minutes`)
+    // }
